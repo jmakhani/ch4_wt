8745cf2b81a9483f710f12ba9ab1bb822309eee9
Jeen <jmakhani@uncc.edu>
2017-03-17 03:34:59 +0000

    CRUD added

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,4 +10,35 @@ class MoviesController < ApplicationController
         @movie = Movie.find(id) # look up movie by unique ID
         # will render app/views/movies/show.html.haml by default
     end
+
+
+
+  def create
+    @movie = Movie.create!(movie_params)
+    flash[:notice] = "#{@movie.title} was successfully created."
+    redirect_to movies_path
+  end
+  
+  def edit
+    @movie = Movie.find params[:id]
+  end
+
+  def update
+    @movie = Movie.find params[:id]
+    @movie.update_attributes!(movie_params)
+    flash[:notice] = "#{@movie.title} was successfully updated."
+    redirect_to movie_path(@movie)
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    flash[:notice] = "Movie '#{@movie.title}' deleted."
+    redirect_to movies_path
+  end
+  
+  def new
+    # default: render 'new' template
+  end
+  
 end
